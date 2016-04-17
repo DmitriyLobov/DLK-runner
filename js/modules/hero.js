83fbe06f84946f0bfbc6fd25138b73cdf695343a
lyoshakravchyk <lyoshakravchyk@gmail.com>
2016-04-17 09:47:39 +0200

    keyup

--- js/modules/hero.js
+++ js/modules/hero.js
@@ -2,18 +2,18 @@ function Hero() {
 	
 	function heroJump() {
 		$('.hero-cont').animate({
-			bottom:'100px'
-		}, 300, function () {
+			bottom:'125px'
+		}, 350, function () {
 			$('.hero-cont').animate({
 				bottom:0
-			},300)
+			},350)
 		})
 	};
 	this.createHero = function() {
 		var hero_cont = $('<div></div>');
 		hero_cont.addClass('hero-cont');
 		hero_cont.appendTo('.game-container');
-		$(window).keypress(function (e) {
+		$(window).keyup(function (e) {
 			if(e.which == 32){
 				heroJump();
 			}
